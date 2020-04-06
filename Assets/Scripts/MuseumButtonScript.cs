546a22b8d0a6bdf086ac2b52eb992d78df07a692
pumibless <tum227@newschool.edu>
2020-04-06 01:50:30 -0400

    added a button in Painting1 to exit painting

---
+++ Assets/Scripts/MuseumButtonScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MuseumButtonScript : MonoBehaviour
+{
+    public void LoadMuseum() {
+        SceneManager.LoadScene("Museum");
+    }
+}
