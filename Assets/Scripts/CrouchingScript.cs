5a7fe3477f7f1f4466ded96d7c6efd20ab4fbb61
pumibless <tum227@newschool.edu>
2020-04-13 04:44:06 -0400

    biotanical scene and salvador dahli scene (check trello for progress)

---
+++ Assets/Scripts/CrouchingScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrouchingScript : MonoBehaviour
+{
+    //private Rigidbody2D rb;
+
+    //access AreaEffector2D components
+    public WindScript windS;
+    
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody2D>();
+    }
+
+
+
+    //Affect WindScript Area Effector Comp.
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {  
+            Debug.Log("A is on");
+            //windS.ae2D.forceMagnitude = 1;
+
+            //rb.isKinematic = true;
+
+        }
+        else {
+            
+            Debug.Log("A is off");
+
+            //rb.isKinematic = false;
+        }
+    }
+}
---
+++ Assets/Scripts/WindScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public AreaEffector2D ae2D;
+    public float moveAmount = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        ae2D = GetComponent<AreaEffector2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //loop wind across screen
+        //transform.position.x < -10.0f
+        rb.velocity = -transform.right * moveAmount;
+
+        //wind control to imitate crouching
+        if (Input.GetKey(KeyCode.A))
+        {
+            ae2D.forceMagnitude = -2;
+        }else {
+            ae2D.forceMagnitude = -6;
+        }
+    }
+
+
+
+}
