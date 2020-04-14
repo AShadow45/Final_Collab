a91935d14b81501ff70c3afb743a761be164105b
Shantanu Nair <shantanunair@me.com>
2020-04-14 16:29:16 -0400

    Deleted move script & now you can enter Dali painting

--- Assets/Scripts/SceneTest.cs
+++ Assets/Scripts/SceneTest.cs
@@ -16,7 +16,7 @@ public class SceneTest : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player"){
-            SceneManager.LoadScene("Salvador Dali");
+            SceneManager.LoadScene("Salvador Dahli");
         }
     }
 }
