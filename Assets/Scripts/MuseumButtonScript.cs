cfc87c6ad892ff1619137338f51f742945170ede
pumibless <tum227@newschool.edu>
2020-04-16 14:25:19 -0400

    botanical scene

--- Assets/Scripts/MuseumButtonScript.cs
+++ Assets/Scripts/MuseumButtonScript.cs
@@ -5,15 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class MuseumButtonScript : MonoBehaviour
 {
+    public GameObject whiteFade;
+
     public void LoadMuseum() {
         SceneManager.LoadScene("Museum");
+    }
+
+    private void Start()
+    {
+        whiteFade.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player")) {
-            LoadMuseum();
+            StartCoroutine(WaitSecToMuseum());
+            whiteFade.SetActive(true);
             //specify location in front of painting
         }
+    }
+
+    IEnumerator WaitSecToMuseum() {
+        yield return new WaitForSeconds(3f);
+        LoadMuseum();
+        
     }
 }
