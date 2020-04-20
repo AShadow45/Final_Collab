daee834228b54e73b3ec272832658eb459bb2a64
pumibless <tum227@newschool.edu>
2020-04-19 23:50:32 -0400

    fixed cursor

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -14,7 +14,9 @@ public class MouseLook : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
     }
 
     // Update is called once per frame
