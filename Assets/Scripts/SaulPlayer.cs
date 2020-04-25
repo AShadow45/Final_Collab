9d53329b899752bf4c5a5817bf3de32f82628009
Shantanu Nair <shantanunair@me.com>
2020-04-25 16:36:55 -0400

    character jumps, but he floats away, how do you fix that?

--- Assets/Scripts/SaulPlayer.cs
+++ Assets/Scripts/SaulPlayer.cs
@@ -2,26 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SaulPlayer : MonoBehaviour
+public class saulPlayer : MonoBehaviour
 {
+    public float speed;
+
+    public float jumpForce;
+
     private Rigidbody2D rb2D;
-    public float moveSpeed;
     private Vector2 moveVelocity;
+
+    public float moveInput;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+
+    public float checkRadius;
+    public LayerMask whatIsGround;
     // Start is called before the first frame update
     void Start()
     {
-       rb2D = GetComponent<Rigidbody2D>(); 
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        moveVelocity = moveInput * moveSpeed;
+        moveInput = Input.GetAxis("Horizontal");
+        moveVelocity = new Vector2(moveInput * speed, rb2D.velocity.y);
+        if(Input.GetKeyDown(KeyCode.Space)){
+            rb2D.velocity = Vector2.up * jumpForce;
+        }
     }
 
     void FixedUpdate()
     {
-        rb2D.MovePosition(rb2D.position + moveVelocity*Time.fixedDeltaTime); 
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        rb2D.MovePosition(rb2D.position + moveVelocity * Time.fixedDeltaTime);
     }
 }
