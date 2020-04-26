509c1dabde3cfb4e1cafe10bd13035352a353166
Shantanu Nair <shantanunair@me.com>
2020-04-26 12:38:40 -0400

    Saul player completely fixed

--- Assets/Scripts/SaulPlayer.cs
+++ Assets/Scripts/SaulPlayer.cs
@@ -6,8 +6,6 @@ public class saulPlayer : MonoBehaviour
 {
     public float speed;
 
-    public float jumpForce;
-
     private Rigidbody2D rb2D;
     private Vector2 moveVelocity;
 
@@ -25,21 +23,13 @@ public class saulPlayer : MonoBehaviour
     {
         moveInput = Input.GetAxis("Horizontal");
         moveVelocity = new Vector2(moveInput * speed, rb2D.velocity.y);
-        if(Input.GetButtonDown("Jump") && isGrounded){
-            rb2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-            isGrounded = false;
-        }
     }
 
     void FixedUpdate()
     {
         rb2D.MovePosition(rb2D.position + moveVelocity * Time.fixedDeltaTime);
     }
-
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        if(other.gameObject.tag == "Ground"){
-            isGrounded = true;
-        }
-    }
 }
+
+ 
+
