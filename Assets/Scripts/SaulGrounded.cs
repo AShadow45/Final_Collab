573dbdc00e2a95544d926aff005d86990408bf50
Shantanu Nair <shantanunair@me.com>
2020-05-03 22:21:29 -0400

    Working Copy w/ all textures & all lvls working

--- Assets/Scripts/SaulGrounded.cs
+++ Assets/Scripts/SaulGrounded.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -27,33 +26,3 @@ public class SaulGrounded : MonoBehaviour
     }
 
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SaulGrounded : MonoBehaviour
-{
-    GameObject Player;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Player = gameObject.transform.parent.gameObject;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void OnCollisionEnter2D(Collision2D other)
-    {
-       if(other.gameObject.tag == "Ground"){
-           Player.GetComponent<SaulPlayer>().isGrounded = true;
-       } 
-    }
-
-}
->>>>>>> 2df62836420f000a83fb5e4e4797387ef1d686f8
